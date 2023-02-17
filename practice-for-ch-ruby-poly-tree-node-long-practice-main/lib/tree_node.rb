398bf0296c0647f3510573b2400d6617347db785
brandon <brandon.w.leung@gmail.com>
2023-02-17 15:24:16 -0800

    changes

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -5,15 +5,47 @@ class PolyTreeNode
     def initialize(value, children = [], parent = nil )
         @value = value
         @children = []
-        @parent = nil
+        @parent = parent
     end
 
     def parent=(node)
+        @parent.children.delete(self) if !@parent.nil?
         @parent = node
-        node.children << self if node != nil && !node.children.include?(self)
+        @parent.children << self if node != nil && !node.children.include?(self)
     end
 
+    def add_child(child_node)
+        @children << child_node if !children.include?(child_node)
+        child_node.parent = self
+    end
     
+    def remove_child(child_node)
+        child_node.parent = nil
+        raise "sorry #{child_node} is not our child" if !children.include?(child_node)
+        @children.delete(child_node) if children.include?(child_node)
+        
+    end
+
+    def dfs(target = nil)
+        return self if target == self.value
+        children.each do |child|
+            result = child.dfs(target)
+            return result unless result.nil?
+        end
+        nil
+    end
+
+    def bfs(target = nil)
+
+        queue = [self]
+        until queue.empty?
+            first = queue.shift
+            return first if first.value == target
+            queue.concat(first.children)
+        end
+
+        nil
+    end
 
 
 end
